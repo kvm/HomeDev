30f30ec05a5a0632c4fc99f59fd0e6422a1a418e
kvm <kvmsravanth@gmail.com>
2015-03-07 00:51:31 +0530

    list binding successful

--- private/Apps/Coursera/WindowsPhone/CourseraNext/App.xaml.cs
+++ private/Apps/Coursera/WindowsPhone/CourseraNext/App.xaml.cs
@@ -1,23 +1,11 @@
-﻿using System;
+﻿using CourseraNext.Managers;
+using System;
+using System.Threading.Tasks;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Animation;
-using Windows.UI.Xaml.Navigation;
-using CourseraNext.Managers;
-using System.Threading.Tasks;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 
@@ -153,7 +141,7 @@ namespace CourseraNext
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                if (!rootFrame.Navigate(typeof(MainPage), e.Arguments))
+                if (!rootFrame.Navigate(typeof(CoursesPage), e.Arguments))
                 {
                     throw new Exception("Failed to create initial page");
                 }
---
+++ private/Apps/Coursera/WindowsPhone/CourseraNext/ViewModels/CoursesPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseraNext.ViewModels
+{
+    public class CoursesPageViewModel
+    {
+        public List<CourseraNext.Models.Course> courses;
+    }
+}
